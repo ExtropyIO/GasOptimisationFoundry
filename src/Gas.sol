37807c9231866301b1b68a2afd04114fce697cf0
susumu tomita <oyster880@gmail.com>
2024-03-12 01:17:53 +0900

    update

--- src/Gas.sol
+++ src/Gas.sol
@@ -4,16 +4,31 @@ pragma solidity 0.8.24;
 contract GasContract {
     uint256 private whiteListAmount;
     mapping(address => uint256) public balances;
-    address[5] public administrators;
     event WhiteListTransfer(address indexed);
     event AddedToWhitelist(address userAddress, uint256 tier);
     constructor(address[] memory _admins, uint256 _totalSupply) {
         balances[0x0000000000000000000000000000000000001234] = 1000000000;
-        administrators[0] = 0x3243Ed9fdCDE2345890DDEAf6b083CA4cF0F68f2;
-        administrators[1] = 0x2b263f55Bf2125159Ce8Ec2Bb575C649f822ab46;
-        administrators[2] = 0x0eD94Bc8435F3189966a49Ca1358a55d871FC3Bf;
-        administrators[3] = 0xeadb3d065f8d15cc05e92594523516aD36d1c834;
-        administrators[4] = 0x0000000000000000000000000000000000001234;
+    }
+
+    function administrators(uint index) public pure returns (address admin) {
+        assembly {
+            switch index
+            case 0 {
+                admin := 0x3243Ed9fdCDE2345890DDEAf6b083CA4cF0F68f2
+            }
+            case 1 {
+                admin := 0x2b263f55Bf2125159Ce8Ec2Bb575C649f822ab46
+            }
+            case 2 {
+                admin := 0x0eD94Bc8435F3189966a49Ca1358a55d871FC3Bf
+            }
+            case 3 {
+                admin := 0xeadb3d065f8d15cc05e92594523516aD36d1c834
+            }
+            case 4 {
+                admin := 0x1234
+            }
+        }
     }
 
     function addToWhitelist(address _userAddrs, uint256 _tier) external {
